36bbfcf626368c4e4cdcab66026c135995f4c997
posternaka <nik.petrenko97@gmail.com>
2026-07-23 23:31:33 +0300

    feat: add react hook form and set form

--- src/screen/AddVideoScreen/AddVideoScreen.tsx
+++ src/screen/AddVideoScreen/AddVideoScreen.tsx
@@ -4,40 +4,54 @@ import { useState } from "react";
 
 import { parseYouTube } from "@/src/shared/libs";
 
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  videoUrl: string;
+};
+
 // https://www.youtube.com/watch?v=oHAmjGo7h58
 
 export const AddVideoScreen = () => {
   const [videoId, setVideoId] = useState("");
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit = (data: Inputs) => {
+    const url = data.videoUrl;
+
+    if (!url) return;
+
+    let finalUrl: URL | null = null;
+
+    try {
+      finalUrl = new URL(url);
+    } catch (error) {
+      console.error(error);
+    }
+
+    if (!finalUrl) return;
+
+    const videoId = parseYouTube(finalUrl);
+
+    if (!videoId) return;
+
+    setVideoId(videoId);
+  };
+
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          const anyE = e as any;
-          const url = anyE.target.elements[0].value;
-
-          if (!url) return;
-
-          let finalUrl: URL | null = null;
-
-          try {
-            finalUrl = new URL(url);
-          } catch (error) {
-            console.error(error);
-          }
-
-          if (!finalUrl) return;
-
-          const videoId = parseYouTube(finalUrl);
-
-          if (!videoId) return;
-
-          setVideoId(videoId);
-        }}
-      >
-        <input type="text" placeholder="Link on Youtube video" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Link on Youtube video"
+          {...register("videoUrl")}
+        />
         <button>Download</button>
       </form>
       <iframe
